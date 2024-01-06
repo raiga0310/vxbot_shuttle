cfa5e75ba722074a5ea885814d78ff3adffaf255
Raiga SASAYAMA <raiga0310brilio@gmail.com>
2024-01-07 00:27:23 +0900

    fix: schema

--- schema.sql
+++ schema.sql
@@ -1,6 +1,8 @@
+CREATE TYPE res_mode AS ENUM ("fixup", "fx", "vx");
+
 CREATE TABLE modes (
     guild_id VARCHAR(20),
     user_id VARCHAR(20),
-    mode ENUM ("fixup", "fx", "vx"),
+    mode res_mode,
     PRIMARY KEY (guild_id, user_id)
-)
+);
